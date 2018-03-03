0cf6c8f71ad00b03a1e2ac620ee4e8b9f351477b
Vincibean <andrebessi00@gmail.com>
2018-03-03 23:00:42 +0100

    code tinkering

---
+++ src/main/scala/org/vincibean/parser/combinators/Main.scala
@@ -0,0 +1,81 @@
+package org.vincibean.parser.combinators
+
+import fastparse.all._
+import fastparse.core
+
+import scala.io.Source
+
+object Main {
+
+  def main(args: Array[String]) {
+
+    // Space
+    val newline = P("\n" | "\r\n" | "\r" | "\f")
+    val space = P(" " | "\t" | newline)
+
+    // String
+    val alpha = P(CharIn(('a' to 'z') ++ ('A' to 'Z'))).rep.!
+    val string = alpha
+
+    // Number
+    val digits = P(CharsWhileIn('0' to '9'))
+    val exponent = P(CharIn("eE") ~ CharIn("+-").? ~ digits)
+    val fractional = P("." ~ digits)
+    val integral = P("0" | CharIn('1' to '9') ~ digits.?)
+
+    val number = P(CharIn("+-").? ~ integral ~ fractional.? ~ exponent.?).!.map(
+      x => x.toDouble
+    )
+
+    // Boolean
+    val `true` = P("true")
+    val `false` = P("false")
+    val yes = P("yes")
+    val no = P("no")
+    // TODO 0 & 1 shouldn't be considered numbers!
+    val as0 = P("0")
+    val as1 = P("1")
+
+    val boolean = P((`true` | `false` | yes | no | as0 | as1).!).map {
+      case "true" | "yes" | "1" => true
+      case "false" | "no" | "0" => false
+    }
+
+    // Path
+    val slash = P("/")
+    val path =
+      (slash ~ alpha).rep.map(x => x.filter(_.nonEmpty))
+
+    // Summary
+    val keyValue = P(CharIn(('a' to 'z') ++ ('A' to 'Z') :+ '_')).rep.!
+    val key = P(keyValue.!)
+    val eq = space ~ "=" ~ space
+    val value = boolean | number | path | string
+
+    val line = P(key ~ eq ~ value ~ newline.rep(1))
+
+    val multiline = line.rep
+
+    val header = P("[" ~ string ~ "]" ~ newline.rep(1))
+
+    val total = P(header ~ multiline ~ newline.rep)
+
+    val multitotal = total.rep
+
+    val mys = P(AnyChar.rep.!)
+
+    // TODO libraryDependencies += "com.jsuereth" %% "scala-arm" % "1.3"
+    println(
+      multitotal.parseIterator(
+        Source
+          .fromFile(
+            "/Users/andrea/IdeaProjects/mine/Parser Combinators/src/main/resources/test_settings.conf")
+          .getLines
+          .map(_ + "\n")))
+
+  }
+
+}
+
+case class Key(ovride: String) extends AnyVal
+case class Value[A](value: A) extends AnyVal
