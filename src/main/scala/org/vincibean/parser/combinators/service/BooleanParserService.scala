2308ca30174497fd1ec33e5bd3866690f81ffcfd
Vincibean <andrebessi00@gmail.com>
2018-03-06 23:23:11 +0100

    Improved BooleanParserService

--- src/main/scala/org/vincibean/parser/combinators/service/BooleanParserService.scala
+++ src/main/scala/org/vincibean/parser/combinators/service/BooleanParserService.scala
@@ -6,10 +6,15 @@ import org.vincibean.parser.combinators.lexical.Ast
 
 trait BooleanParserService {
 
-  val booleanParser: core.Parser[Ast.SingleVal[Boolean], Char, String] =
-    P(StringIn("true", "false", "yes", "no", "0", "1").!).map {
+  val booleanParser: core.Parser[Ast.SingleVal[Boolean], Char, String] = {
+    val number = P(CharsWhileIn(('0' to '9') :+ '.'))
+    val as0 = P("0" ~ !number)
+    val as1 = P("1" ~ !number)
+    val others = P(StringIn("true", "false", "yes", "no"))
+    P(others | as0 | as1).!.map {
       case "true" | "yes" | "1" => Ast.True
       case "false" | "no" | "0" => Ast.False
     }
+  }
 
 }
