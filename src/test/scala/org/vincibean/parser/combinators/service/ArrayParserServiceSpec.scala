4112a4cccda7a2851c235db02fb7fa657e2761ab
Vincibean <andrebessi00@gmail.com>
2018-03-07 00:36:35 +0100

    fixed typo and improved tests in ArrayParserServiceSpec

--- src/test/scala/org/vincibean/parser/combinators/service/ArrayParserServiceSpec.scala
+++ src/test/scala/org/vincibean/parser/combinators/service/ArrayParserServiceSpec.scala
@@ -17,8 +17,9 @@ class ArrayParserServiceSpec
         ArrayParserService can
           parse any String array $p1
           parse any Double array $p2
-          parse any Boolean array $p2
-          parse any Path array $p2
+          parse any Boolean array (case of true) $p3
+          parse any Boolean array (case of false) $p4
+          parse any Path array $p5
     """
 
   val p1 = {
@@ -45,18 +46,34 @@ class ArrayParserServiceSpec
   }
 
   val p3 = {
-    implicit val ass: Arbitrary[Seq[String]] = Arbitrary(
-      Gen.listOf(Gen.oneOf(Seq("true", "yes", "1", "false", "no", "0"))))
+    implicit val bs: Arbitrary[Seq[String]] = Arbitrary(
+      Gen.listOf(Gen.oneOf(Seq("true", "yes", "1"))))
     prop { (a: Seq[String]) =>
       (a.length > 1) ==> {
         val res = arrayParser.parse(a.mkString(","))
         (res must beAnInstanceOf[Parsed.Success[Seq[Boolean]]]) and (res.get.value.wrapped
-          .map(_.wrapped) must beEqualTo(a))
+          .map(_.wrapped)
+          .map(_.asInstanceOf[Boolean])
+          .reduce(_ && _) must beEqualTo(true))
       }
     }
   }
 
   val p4 = {
+    implicit val bs: Arbitrary[Seq[String]] = Arbitrary(
+      Gen.listOf(Gen.oneOf(Seq("false", "no", "0"))))
+    prop { (a: Seq[String]) =>
+      (a.length > 1) ==> {
+        val res = arrayParser.parse(a.mkString(","))
+        (res must beAnInstanceOf[Parsed.Success[Seq[Boolean]]]) and (res.get.value.wrapped
+          .map(_.wrapped)
+          .map(_.asInstanceOf[Boolean])
+          .reduce(_ && _) must beEqualTo(false))
+      }
+    }
+  }
+
+  val p5 = {
     prop { (a: Seq[JPath]) =>
       (a.length > 1) ==> {
         val res = arrayParser.parse(a.mkString(","))
