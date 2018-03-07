8068ff5da1c7c2e2660f76f16eb67aec9cd86e13
Vincibean <andrebessi00@gmail.com>
2018-03-07 01:49:21 +0100

    Added test for ParserService

---
+++ src/test/scala/org/vincibean/parser/combinators/service/ParserServiceSpec.scala
@@ -0,0 +1,26 @@
+package org.vincibean.parser.combinators.service
+
+import org.specs2.Specification
+import org.specs2.specification.core.SpecStructure
+
+import scala.util.Try
+
+class ParserServiceSpec extends Specification with ParserService {
+
+  val config = loadConfig(
+    this.getClass.getClassLoader.getResource("settings.conf").getFile,
+    List("ubuntu" -> "production"))
+
+  val invalidConfig = loadConfig(
+    this.getClass.getClassLoader.getResource("invalid-settings.conf").getFile,
+    List("ubuntu" -> "production"))
+
+  override def is: SpecStructure =
+    s2"""
+        ParserService can
+          successfully parse a complex settings file ${config.groupSettings must not beEmpty}
+          throw an exception when a non-existing file is given ${Try(
+      loadConfig("i-should-not-exist.conf", Nil)) must beFailedTry}
+          return an empty config when an invalid settings file is given ${invalidConfig.groupSettings must beEmpty}
+      """
+}
